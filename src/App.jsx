2dcdf0ae2ef86dd2ac76cce13eac54ad869c947a
Sandil Adhikari <adhikarisandil32@gmail.com>
2023-06-08 17:53:54 +0545

    .

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import SearchBoxContainer from './components/SearchBoxContainer'
 import MovieCardsCollection from './components/MovieCardsCollection'
 
 export default function App() {
-  console.log(`${import.meta.env.VITE_TEST_STRING}`)
   return (
     <div className="min-h-screen bg-slate-900">
       <SearchBoxContainer />
