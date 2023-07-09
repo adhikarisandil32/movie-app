e846001b35ff7ec0634f7774dd73970dd9877347
Sandil Adhikari <adhikarisandil32@gmail.com>
2023-07-09 17:49:38 +0545

    publish removed

--- functions/serverRequest.js
+++ functions/serverRequest.js
@@ -4,8 +4,9 @@ export const handler = async (event, context) => {
 
   try {
     const searchParams = new URLSearchParams(event.queryStringParameters).toString();
+    console.log(searchParams)
     const response = await axios.get(`${process.env.VITE_BASE_URL}${searchParams}`)
-    
+    console.log(`${process.env.VITE_BASE_URL}${searchParams}`)
     return {
       statusCode: 200,
       body: JSON.stringify(response.data)
